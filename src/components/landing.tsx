0601d7fea5af8ce67f585b5b62e28913e119b0aa
Johannes Naylor <jonaylor89@gmail.com>
2023-03-09 14:32:06 +0100

    add tapped logo

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -1,23 +1,23 @@
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Landing() {
-    const [buttonColor, setButtonColor] = useState("transparent")
-
-    const handleScroll = () => {
-        const top = window.pageYOffset || 0;
-        if (top > 60) {
-            setButtonColor("transparent")
-        }
-    }
 
     return (
         <>
-            <div 
-                id="home" 
+            <div
+                id="home"
                 className="flex h-screen"
-                onScroll={handleScroll}
-                >
+            >
                 <div className="m-auto">
+                    <div className="flex justify-center">
+                        <Image
+                            src="/images/tapped_reverse.png"
+                            alt="tapped logo"
+                            width={300}
+                            height={300}
+                        />
+                    </div>
                     <h1 className="font-bold py-20 text-2xl lg:text-6xl">
                         Know who you&apos;re working with
                     </h1>
