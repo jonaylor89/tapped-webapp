7fa0098486472cd49f44e7787efa665325ad7962
Johannes Naylor <jonaylor89@gmail.com>
2023-05-09 16:15:54 -0400

    styling

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -22,7 +22,7 @@ export default function Landing() {
                     <div className="flex justify-center font-bold md:text-6xl pr-8 pl-8 md:pr-32 md:pl-32 text-center">
                     network with professionals, secure gigs, and promote your portfolio.
                     </div>
-                    <div className="flex justify-center md:text-2xl pt-8">
+                    <div className="flex justify-center md:text-2xl pt-8 pr-8 pl-8 text-center">
                         raising $300,000 💰 located in nyc 📍 1.5k users 🫂 live on app store this summer 📱
                     </div>
                     <div className="grid grid-cols-1 lg:grid-cols-2 md:mt-12 mr-30 ml-30">
