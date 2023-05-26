1bc8ccd579baff46ebdae77acd2df501352937eb
Johannes Naylor <jonaylor89@gmail.com>
2023-05-26 14:55:51 +0200

    styling

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -29,7 +29,7 @@ export default function Landing() {
 
                         <div className="flex justify-center items-center flex-col md:flex-row gap-4 md:justify-start">
                             <Link href="/download">
-                                <button className="bg-gray-800 hover:bg-gray-400 text-gray-300 hover:text-gray-800 font-bold py-2 px-4 rounded-full inline-flex items-center">
+                                <button className="bg-gray-800 hover:bg-gray-400 text-gray-300 hover:text-gray-800 font-bold py-2 px-4 rounded-full inline-flex items-center duration-300">
                                     <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
                                     <span>download</span>
                                 </button>
